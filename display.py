4375c2e43e1b79d49835e57b98e506958decea65
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2021-10-17 00:23:05 +0800

    refactor(main): abstract to display function

--- display.py
+++ display.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+import cv2
+
 from threads.cam_thread import camThread
 from threads.screen_thread import screenThread
 
@@ -5,3 +9,11 @@ from threads.screen_thread import screenThread
 
 qr_thread = camThread("IQT QR Detector", 0)
 screen_thread = screenThread("Screen")
+
+def display(qr_frame, screen_frame):
+  h1, w1 = qr_frame.shape[:2]
+  h2, w2 = screen_frame.shape[:2]
+  vis = np.zeros((h1 + h2, max(w1, w2), 4), np.uint8)
+  vis[:h1, :w1, :3] = qr_frame
+  vis[h1:h1+h2, :w2, :4] = screen_frame
+  cv2.imshow("IQT Entrance", vis)
